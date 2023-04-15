680c9aaa0d90cc77abe944fdfe2e25bd8651e07c
Rohith Tripuraneni <rohithchowdary70@gmail.com>
2023-04-15 20:53:07 +0000

    implemented loading skeleton for the spy card

--- src/components/SpyCard.tsx
+++ src/components/SpyCard.tsx
@@ -4,32 +4,36 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import SpyCardSkeleton from "./SpyCardSkeleton";
 
 interface SpyCardProps {
   isLoading: boolean;
   spyData: spy;
 }
 export default function SpyCard({ isLoading = false, spyData }: SpyCardProps) {
-  const {
-    name: { first = "", last = "", title = "" },
-    picture: { large = "" },
-  } = spyData || {};
+  const { name, picture } = spyData || { name: {} };
   return (
     <Card sx={{ maxWidth: "100%" }}>
-      <CardMedia sx={{ height: 386 }} image={large} title={title} />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
+      {isLoading ? (
+        <SpyCardSkeleton />
+      ) : (
+        <>
+          <CardMedia sx={{ height: 386 }} image={picture?.large} title={""} />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Lizard
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Lizards are a widespread group of squamate reptiles, with over
+              6,000 species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </>
+      )}
     </Card>
   );
 }
